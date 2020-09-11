96447f1e1076a3701ec63e784e7c5d59629d8793
SourGaeko <66372255+SourGaeko@users.noreply.github.com>
2020-09-11 20:50:20 +0900

    20200911 Join 2

---
+++ db.js
@@ -0,0 +1,50 @@
+export const dbVideos = [
+    {
+        id:324393,
+        title: "Big Buck Bunny",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creator: {
+            id: 121212,
+            name: "Kiyoung Jang",
+            email: "rldud921209@naver.com",
+        }
+    },
+    {
+        id:143513,
+        title: "Elephant Dream",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        creator: {
+            id: 121212,
+            name: "Kiyoung Jang",
+            email: "rldud921209@naver.com",
+        }
+    },
+    {
+        id:736542,
+        title: "For Bigger Blazes",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        creator: {
+            id: 121212,
+            name: "Kiyoung Jang",
+            email: "rldud921209@naver.com",
+        }
+    },
+    {
+        id:351464,
+        title: "For Bigger Escapes",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+        creator: {
+            id: 121212,
+            name: "Kiyoung Jang",
+            email: "rldud921209@naver.com",
+        }
+    }
+]
