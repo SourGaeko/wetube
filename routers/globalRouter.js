6d5b0feddf5b7af5c0942f079a0a7eed39b6ad8b
SourGaeko <66372255+SourGaeko@users.noreply.github.com>
2020-09-11 20:48:47 +0900

    20200911 Join

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
 import routes from "../routes";
-import { videoHome, searchVideos } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import { home, searchVideos } from "../controllers/videoController";
+import { getJoin, postJoin, login, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, videoHome);
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, searchVideos);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
